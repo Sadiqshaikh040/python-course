bc7d9cbfffef3227c06d4a1593afcbb984180790
Sadiqshaikh040 <sadiqshaikh040@gmail.com>
2024-03-16 14:23:54 +0530

    added new files and completeled the exercise

---
+++ solutions_python_notion/03_Day_Operators/operators1.py
@@ -0,0 +1,20 @@
+#17
+# num=int(input("enter number"))
+num=2
+if (num %2==0):
+    print("even number")
+else:
+    print("odd number")
+#18
+fdiv=7//3
+print(10==int(9.8))
+#21
+enter_hour=int(input("no of hours"))
+rate_per_hour=int(input("pay out per hour"))
+print("per day earning:",enter_hour*rate_per_hour)
+#22
+enter_year=int(input("no of year"))
+print("total no of sec:",enter_year*86400*365)
+#23
+for num in range(1,6):
+    print(num,num**0,num**1,num**2,num**3)
